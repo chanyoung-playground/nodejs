a21760e522dd7186a071d4671484350333b584e4
dokimion24 <leecy3610@gmail.com>
2023-09-23 15:53:23 +0900

    express

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
-const http = require('http');
-const routes = require('./routes');
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
 
-const server = http.createServer(routes.handler);
+const app = express();
 
-server.listen(3000);
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, 'views', 'error.html'));
+});
+
+app.listen(3000);
